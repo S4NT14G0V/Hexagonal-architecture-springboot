5f5e876f8507d5cf60d96f336a8284d01e0d1918
S4NT14G0V <santiagot3p@gmail.com>
2026-08-23 12:03:05 -0500

    feat: implement GetByIdUserService use case

---
+++ src/main/java/com/backend/hexagonal/application/service/GetByIdUserService.java
@@ -0,0 +1,22 @@
+package com.backend.hexagonal.application.service;
+
+import com.backend.hexagonal.application.exception.UserNotFoundException;
+import com.backend.hexagonal.application.port.in.GetByIdUserUseCase;
+import com.backend.hexagonal.application.port.out.UserRepositoryPort;
+import com.backend.hexagonal.domain.model.User;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class GetByIdUserService implements GetByIdUserUseCase {
+
+    @Autowired
+    private UserRepositoryPort userRepositoryPort;
+
+    @Override
+    public User execute(UUID id) {
+        return userRepositoryPort.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("User with ID " + id + " not found"));
+    }
+}
